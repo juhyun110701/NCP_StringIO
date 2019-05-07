a8c4423a8b0e885bd6214575b17c005c98f374c9
juhyun110701 <s2017s16@e-mirim.hs.kr>
2019-05-07 12:10:05 +0900

    #2 사용자가 입력을 한 후 enter키를 누르면 결과 출력

--- StringIO_TextView/Form1.Designer.cs
+++ StringIO_TextView/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.txtEdit.Name = "txtEdit";
             this.txtEdit.Size = new System.Drawing.Size(231, 21);
             this.txtEdit.TabIndex = 2;
+            this.txtEdit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdit_KeyPress);
             // 
             // Form1
             // 
