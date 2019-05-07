f41a804f6392130779b690bb291e5f290b6aee55
juhyun110701 <s2017s16@e-mirim.hs.kr>
2019-05-07 11:32:07 +0900

    #1 Form Design 작업

--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -15,5 +15,15 @@ namespace StringIO_TextView
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
